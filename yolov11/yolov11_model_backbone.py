8cf8f4606eeef9c1a1e4f1cb53fa383cb2ccf4e6
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-11-14 11:08:41 +0800

    BUilding yolov11 architecture

--- yolov11/yolov11_model_backbone.py
+++ yolov11/yolov11_model_backbone.py
@@ -14,11 +14,11 @@ back_bone_architecture = {
     # CNN layer list with repeat num ['operation_name', (in, out, k, s, p), repeat_num]
     ['Conv', (3, 64, 3, 2, 1), 1],
     ['Conv', (64, 128, 3, 2, 1), 1],
-    ['conv', (128, 128, 3, 2, 1), 3 * R],
+    ['C3K2', (128, 128, 3, 1, 1), 3 * R],
     ['Conv', (128, 256, 3, 2, 1), 1],
-    ['Conv',(256, 256, 3, 2, 1), 3 * R],
+    ['C3K2',(256, 256, 3, 1, 1), 6 * R],
     ['Conv', (256, 512, 3, 2, 1), 1],
-    ['Conv', (512, 512, 3, 2, 1), 3 * R],
+    ['C3K2', (512, 512, 3, 1, 1), 6 * R],
     ['Conv', (512, 1024, 3, 2, 1), 1]
 }
 
@@ -28,9 +28,16 @@ neck_architecture = {
     # Up sample list: ['operation_name', output_size(D, H, W), scale_factor]
     ['Upsample', (512, 40, 40)],
     # Concat list: ['operation_name', dimension, (tensor1, tensor2)]
-    ["Concat", 0, ('back_bone_c3k2_3', 'neck_Upsample_1')],
-    ['Conv', (512, 512, 3, 2, 1), 3 * R],
+    ["Concat", 2,],
+    ['C3K2', (512, 512, 3, 1, 1), 3 * R],
     ['Upsample', (256, 80, 80)],
-    ["Concat", 0, ('back_bone_c3k2_3', 'neck_Upsample_2')],
-    ['Conv', (256, 256, 3, 2, 1), 3 * R],
+    ["Concat", 2],
+    ['C3K2', (256, 256, 3, 2, 1), 3 * R],
+    ['Conv', (256, 512, 3, 2, 1), 1],
+    ["Concat", 2],
+    ['C3K2', (512, 512, 3, 2, 1), 3 * R],
+    ['Conv', (512, 1024, 3, 2, 1), 1],
+    ['Concat', 2],
+    ['C3K2', (1024, 1024, 3, 1, 1), 3 * R]
 }
+
